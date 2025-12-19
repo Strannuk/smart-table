aa721c127a964a1a900c8edfdf35e2a84d1389ad
Kirill Belokamenskyi <belokamensky.kirill@yandex.ru>
2025-12-19 18:24:37 +0300

    Шаг 1. Внедряем мок данные

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import { initSearching } from "./components/searching.js";
 
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const API = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -38,15 +38,16 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let query = {}; 
     // @todo: использование 
     // result = applySearch(result, state, action); 
     // result = applyFiltering(result, state, action);
     // result = applySorting(result, state, action);
     // result = applyPagination(result, state, action);
-    sampleTable.render(result)
+    const { total, items } = await API.getRecords(query);
+    sampleTable.render(items);
 }
 
 const sampleTable = initTable({
@@ -84,4 +85,8 @@ const applySorting = initSorting([
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await API.getIndexes()
+}
+
+init().then(render);
